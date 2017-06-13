9add27cf61b5848f19b4a25e55dce48d2399d85f
olerom <mefiader@gmail.com>
2017-06-13 04:03:57 +0300

    Finish describing: 2 variants to solve WORK entity problem

--- sources/queries/task8.sql
+++ sources/queries/task8.sql
@@ -1,30 +1,42 @@
 SELECT
-  c.НОМЕР_ЦЕХА                            AS "Номер цеха",
-  'Производственный'                      AS "Название цеха",
-  COUNT(DISTINCT v.НАЗВАНИЕ_ВИДА_ИЗДЕЛИЯ) AS "Кол-во видов изделий",
-  i.middle                                AS "Среднее кол-во"
-FROM CWПРОИЗВОДСТВ_ЦЕХ c
-  JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
-    ON k.НОМЕР_ЦЕХА = c.НОМЕР_ЦЕХА
-  JOIN CWВИД_ИЗДЕЛИЯ v
-    ON v.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ
-  CROSS JOIN (SELECT AVG(amount) middle
-              FROM (
-                SELECT count(DISTINCT v.НАЗВАНИЕ_ВИДА_ИЗДЕЛИЯ) amount
-                FROM CWЦЕХ c
-                  LEFT JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
-                    ON k.НОМЕР_ЦЕХА = c.НОМЕР_ЦЕХА
-                  LEFT JOIN CWВИД_ИЗДЕЛИЯ v
-                    ON v.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ
-                GROUP BY c.НОМЕР_ЦЕХА)) i
-GROUP BY c.НОМЕР_ЦЕХА, i.middle
-HAVING COUNT(DISTINCT v.НАЗВАНИЕ_ВИДА_ИЗДЕЛИЯ) > (
-  SELECT AVG(amount)
+  mc.НОМЕР_ЦЕХА,
+  mc.НАЗВАНИЕ_ЦЕХА,
+  COUNT(*) AS "Кол-во видов"
+FROM CWПРОИЗВОДСТВЕННЫЙ_ЦЕХ c
+  INNER JOIN CWЦЕХ mc
+    ON c.НОМЕР_ЦЕХА = mc.НОМЕР_ЦЕХА
+  INNER JOIN CWТИП_КАТЕГОРИИ t
+    ON c.НОМЕР_ЦЕХА = t.НОМЕР_ЦЕХА
+  INNER JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
+    ON k.ТИП_КАТЕГОРИИ = t.ТИП_КАТЕГОРИИ
+  INNER JOIN CWВИД_ИЗДЕЛИЯ v
+    ON v.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ
+GROUP BY mc.НОМЕР_ЦЕХА, mc.НАЗВАНИЕ_ЦЕХА
+HAVING COUNT(*) > (
+  SELECT AVG(count)
   FROM (
-    SELECT count(DISTINCT v.НАЗВАНИЕ_ВИДА_ИЗДЕЛИЯ) amount
-    FROM CWЦЕХ c
-      LEFT JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
-        ON k.НОМЕР_ЦЕХА = c.НОМЕР_ЦЕХА
-      LEFT JOIN CWВИД_ИЗДЕЛИЯ v
-        ON v.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НАЗВАНИЕ_КАТЕГОРИИ_ИЗДЕЛИЯ
-    GROUP BY c.НОМЕР_ЦЕХА));
+    SELECT COUNT(*) count
+    FROM CWПРОИЗВОДСТВЕННЫЙ_ЦЕХ C
+      INNER JOIN CWТИП_КАТЕГОРИИ t
+        ON C.НОМЕР_ЦЕХА = t.НОМЕР_ЦЕХА
+      INNER JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
+        ON k.ТИП_КАТЕГОРИИ = t.ТИП_КАТЕГОРИИ
+      INNER JOIN CWВИД_ИЗДЕЛИЯ v
+        ON v.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ
+    GROUP BY C.НОМЕР_ЦЕХА))
+UNION ALL
+SELECT
+  'Среднее количество: ' || AVG(count),
+  NULL,
+  NULL
+FROM (
+  SELECT COUNT(*) count
+  FROM CWПРОИЗВОДСТВЕННЫЙ_ЦЕХ C
+    INNER JOIN CWТИП_КАТЕГОРИИ t
+      ON C.НОМЕР_ЦЕХА = t.НОМЕР_ЦЕХА
+    INNER JOIN CWКАТЕГОРИЯ_ИЗДЕЛИЯ k
+      ON k.ТИП_КАТЕГОРИИ = t.ТИП_КАТЕГОРИИ
+    INNER JOIN CWВИД_ИЗДЕЛИЯ v
+      ON v.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ = k.НОМЕР_КАТЕГОРИИ_ИЗДЕЛИЯ
+  GROUP BY C.НОМЕР_ЦЕХА)
+
